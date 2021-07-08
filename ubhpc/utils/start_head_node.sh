8ac5d9f122c8ef33ace030dcf6999ab610848db1
williamrmathias45@gmail.com <williamrmathias45@gmail.com>
2021-07-08 19:59:13 +0000

    added files necessary for slurm ctrlr

---
+++ ubhpc/utils/start_head_node.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+export SLURM_HOME=/opt/slurm
+export SLURM_CONF=/opt/cluster/ubhpc/etc/slurm.conf
+export PATH=${SLURM_HOME}/sbin:${SLURM_HOME}/bin:${PATH}
+
+echo "Dropping slurmdb_ubhpc DB"
+mysql -u root << END
+drop database if exists slurmdb_ubhpc;
+END
+
+echo "Starting slurmdbd"
+slurmdbd
+echo "Starting slurmctld"
+slurmctld
+
+sleep 10
+echo "Running sacctmgr"
+set +e
+sacctmgr -i < /opt/cluster/ubhpc/utils/sacctmgr.script
+set -e
+
+ps -Af
