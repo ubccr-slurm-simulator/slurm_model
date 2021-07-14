2fff22e569ab0c11375a188b4c4887c2ba9e70b5
williamrmathias45@gmail.com <williamrmathias45@gmail.com>
2021-07-14 20:23:06 +0000

    added more nodes and jobs; need more addresses

--- ubhpc/utils/start_compute_node.sh
+++ ubhpc/utils/start_compute_node.sh
@@ -3,7 +3,12 @@ export SLURM_HOME=/opt/slurm
 export SLURM_CONF=/opt/cluster/ubhpc/etc/slurm.conf
 export PATH=${SLURM_HOME}/sbin:${SLURM_HOME}/bin:${PATH}
 
-mkdir /opt/cluster/ubhpc/log
+mkdir -p /opt/cluster/ubhpc/run /opt/cluster/ubhpc/log
+mkdir -p /opt/cluster/ubhpc/var/spool /opt/cluster/ubhpc/var/state
+chown -R slurm:slurm /opt/cluster
+chmod 755 /opt/cluster /opt/cluster/ubhpc /opt/cluster/ubhpc/var
+chmod 700 /opt/cluster/ubhpc/var/spool /opt/cluster/ubhpc/var/state
+chown -R slurm:slurm /opt
 
 echo "Starting slurmd"
-#slurmd
+slurmd
