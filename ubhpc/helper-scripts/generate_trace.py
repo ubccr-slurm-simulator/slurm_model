c353e7ba06e97b04e9be270c6eb3e87432b045c9
williamrmathias45@gmail.com <williamrmathias45@gmail.com>
2021-07-20 17:50:20 +0000

    generated events and helper script

---
+++ ubhpc/helper-scripts/generate_trace.py
@@ -0,0 +1,71 @@
+import sys
+import time
+import pandas as pd
+import datetime as dt
+
+filename = sys.argv[1]
+outfilename = sys.argv[2]
+
+def generate_trace(filename, outfilename):
+    infile = open(filename, "r")
+    BeginTime = dt.datetime(2021, 6, 1, 00, 00, 00)
+
+    header = infile.readline()
+    header = header.split("|")
+    header[-1] = header[-1].strip()
+
+    SubmitTime = []
+    JobID = []
+    WallTime = []
+    User = []
+    RequestTime = []
+    NumNodes = []
+    NumTasksPerNode = []
+    Account = []
+    Partition = []
+
+    df = pd.DataFrame()
+
+    lines = infile.readlines()[:-2]
+    for line in lines:
+        line = line.split("|")
+
+        if line[11] == "Unknown":
+            continue
+
+        line[9] = dt.datetime.strptime(line[9], "%Y-%m-%dT%H:%M:%S")
+        line[11] = dt.datetime.strptime(line[11], "%Y-%m-%dT%H:%M:%S")
+        line[12] = dt.datetime.strptime(line[12], "%Y-%m-%dT%H:%M:%S")
+
+        SubmitTime.append((line[9] - BeginTime).total_seconds())
+        JobID.append(line[0])
+        WallTime.append((line[12] - line[11]).total_seconds())
+        User.append(line[7])
+        RequestTime.append(line[-3])
+        NumNodes.append(line[16])
+        NumTasksPerNode.append(int(int(line[17]) / int(line[16])))
+        Account.append(line[4])
+        Partition.append(line[3])
+    infile.close()
+
+    df["SubmitTime"] = SubmitTime
+    df["JobID"] = JobID
+    df["WallTime"] = WallTime
+    df["User"] = User
+    df["RequestTime"] = RequestTime
+    df["NumNodes"] = NumNodes
+    df["NumTasksPerNode"] = NumTasksPerNode
+    df["Account"] = Account
+    df["Partition"] = Partition
+
+    outfile = open(outfilename, "w")
+    for index, row in df.iterrows():
+        outfile.write("-dt " + str(row["SubmitTime"]) + " -e " + "submit_batch_job "
+                        + "| " + "-jid " + str(row["JobID"]) + " -sim-walltime "
+                        + str(row["WallTime"]) + " --uid=" + str(row["User"])
+                        + " -t " + str(row["RequestTime"]) + " -n " + str(row["NumNodes"])
+                        + " --ntasks-per-node=" + str(row["NumTasksPerNode"]) + " -A "
+                        + str(row["Account"]) + " -p " + str(row["Partition"]) + " -q "
+                        + str(row["Partition"]) + " pseudo.job\n")
+
+generate_trace(filename, outfilename)
